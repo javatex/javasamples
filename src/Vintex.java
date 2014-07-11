f8210b2774e578aeed57a02193fafd9d94937286
javatex <ordinarcev96@yandex.ru>
2014-07-11 15:06:35 +0400

    commit secoder

--- src/Vintex.java
+++ src/Vintex.java
@@ -1,9 +1,9 @@
 public class Vintex {
     public static void main(String[] args) {
-        // TODO testing Wolfram Alpha
         for (int i = 0; i < 15; i++) {
             System.out.print((i*i*i+2*i*i-i+1)+", ");
         }
-        System.out.print("...");
+        System.out.println("...");
+        System.out.print("What can i do?");
     }
 }
