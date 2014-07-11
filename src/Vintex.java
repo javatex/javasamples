95c9afa03901941fc7ef198bf9deb5d7898268f7
javatex <ordinarcev96@yandex.ru>
2014-07-11 12:22:11 +0400

    readme update

--- src/Vintex.java
+++ src/Vintex.java
@@ -1,6 +1,6 @@
 public class Vintex {
     public static void main(String[] args) {
-        double x = 4.0;
+        double x = 8.0;
         double y = Math.sqrt(x);
         System.out.println(y);
     }
