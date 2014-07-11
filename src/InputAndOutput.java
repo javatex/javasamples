2dc669bd76cd05ddb7693c968addb5f2cd548082
javatex <ordinarcev96@yandex.ru>
2014-07-11 12:53:43 +0400

    adding input

---
+++ src/InputAndOutput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class InputAndOutput {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your name?");
+        String name = in.nextLine();
+        System.out.println(namea);
+    }
+}
